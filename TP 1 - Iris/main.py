ee826b84ef5aff8c0b139a804423e105fbc70325
Alexandre LAM <alexandrelam@outlook.com>
2021-02-12 18:09:18 +0100

    reformat

--- TP 1 - Iris/main.py
+++ TP 1 - Iris/main.py
@@ -1,18 +1,18 @@
 from perceptron import Perceptron
 from sklearn.model_selection import train_test_split
-from utils import * 
+from utils import *
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-t = [[1,2,3],[3,4,5]]
+t = [[1, 2, 3], [3, 4, 5]]
 
 p = Perceptron(4, 50000, 0.01)
 
-df = pd.read_csv("iris.csv")
+df = pd.read_csv("./data/iris.data")
 df = df[df.species != "virginica"]
 
-train, test = train_test_split(df,test_size=0.2)
+train, test = train_test_split(df, test_size=0.3)
 trainX = train.drop(['species'], axis=1).to_numpy()
 trainY = train['species'].astype('category')
 trainY = trainY.cat.codes
@@ -29,24 +29,23 @@ testX_flatten = testX.reshape(testX.shape[0], -1).T
 trainX_flatten = (trainX_flatten/trainX_flatten.max())
 testX_flatten = (testX_flatten/testX_flatten.max())
 
-W, b, cost = p.fit(trainX_flatten, trainY, True)
+#W, b, cost = p.fit(trainX_flatten, trainY, True)
 
-x = np.arange(1,51)
+x = np.arange(1, 51)
 
-np.savetxt("weights.txt", W)
-np.savetxt("cost.txt", cost)
-saveBias(b, "bias.txt")
+"""
+np.savetxt("./save/weights.txt", W)
+np.savetxt("./save/cost.txt", cost)
+saveBias(b, "./save/bias.txt")
+"""
 
+W = np.loadtxt("./save/weights.txt")
+cost = np.loadtxt("./save/cost.txt")
+b = loadBias("./save/bias.txt")
 
-W = np.loadtxt("weights.txt")
-cost = np.loadtxt("cost.txt")
-b = loadBias("bias.txt")
+predictions = p.forward(testX_flatten, W, b)
 
-print(p.forward(testX_flatten, W, b))
-print(testY)
-
-print(cost)
+print("accuracy: ", accuracy(testY, predictions))
 
 plt.plot(x, cost)
 plt.show()
-
--- TP 1 - Iris/utils.py
+++ TP 1 - Iris/utils.py
@@ -4,8 +4,18 @@ def loadBias(filename):
 
     return float(res)
 
+
 def saveBias(bias, filename):
     with open(filename, "w") as f:
         f.write(str(bias))
 
 
+def accuracy(testY, predictions):
+    count = 0.0
+    testY = testY.to_numpy()
+    for i in range(len(predictions)):
+        if predictions[i] > 0.5 and testY[i]:
+            count += 1
+        if predictions[i] < 0.5 and testY[i] == 0:
+            count += 1
+    return count / len(testY)
